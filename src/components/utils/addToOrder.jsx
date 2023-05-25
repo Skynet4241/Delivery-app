9affca87c9da69077bf5169678341014d94b1321
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-05-25 18:14:41 +0300

    Update addToOrder.jsx

--- src/components/utils/addToOrder.jsx
+++ src/components/utils/addToOrder.jsx
@@ -1,21 +1,3 @@
-// export const addToOrder = (item, setOrder, order) => {
-//   const existingItem = order.find(orderItem => orderItem._id === item._id);
-//   let updatedOrder = [];
-
-//   if (existingItem) {
-//     updatedOrder = order.map(orderItem => {
-//       if (orderItem._id === item._id) {
-//         return { ...orderItem, quantity: orderItem.quantity + 1 };
-//       }
-//       return orderItem;
-//     });
-//   } else {
-//     updatedOrder = [...order, { ...item, quantity: 1 }];
-//   }
-
-//   setOrder(updatedOrder);
-// };
-
 export const addToOrder = (item, setOrder, order) => {
   const existingItem = order.find(orderItem => orderItem._id === item._id);
   let updatedOrder = [];
