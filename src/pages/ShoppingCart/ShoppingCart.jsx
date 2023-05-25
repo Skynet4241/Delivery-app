17b4ca97b7827cbd925c733ddcf53e8f20afbfd1
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-05-25 17:55:59 +0300

    update ShoppingCart func

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,38 +1,70 @@
 import { useEffect, useState } from 'react';
+import { addToOrder } from 'components/utils/addToOrder';
+import { removeItemFromOrder } from 'components/utils/removeItemFromOrder';
+import { OrderList } from './ShoppingCart.styled';
 
 export const ShoppingCart = () => {
   const savedOrder = localStorage.getItem('Order');
-  const order = savedOrder ? JSON.parse(savedOrder) : [];
+  const initialOrder = savedOrder ? JSON.parse(savedOrder) : [];
 
-  const [cartUpdated, setCartUpdated] = useState(false);
   const [quantity, setQuantity] = useState({});
+  const [order, setOrder] = useState(initialOrder);
 
   const clearCart = () => {
     localStorage.removeItem('Order');
-    setCartUpdated(true);
+    setOrder([]);
   };
 
   const handleDelete = item => {
-    const updatedOrder = order.filter(orderItem => orderItem !== item);
-    const updatedQuantity = { ...quantity };
-    delete updatedQuantity[item];
-    localStorage.setItem('Order', JSON.stringify(updatedOrder));
-    setQuantity(updatedQuantity);
-    setCartUpdated(true);
+    removeItemFromOrder(item._id, setOrder, order);
   };
 
-  useEffect(() => {}, [cartUpdated]);
-  const getQuantity = itemId => {
-    const item = order.find(orderItem => orderItem._id === itemId);
-    return item ? item.quantity : 0;
+  const handleDecrease = item => {
+    const updatedOrder = [...order];
+    const updatedQuantity = { ...quantity };
+
+    if (updatedQuantity[item._id] === 1) {
+      removeItemFromOrder(item._id, setOrder, order);
+      delete updatedQuantity[item._id];
+    } else {
+      updatedOrder.forEach(orderItem => {
+        if (orderItem._id === item._id) {
+          orderItem.quantity -= 1;
+          updatedQuantity[item._id] -= 1;
+        }
+      });
+    }
+
+    setQuantity(updatedQuantity);
   };
+
+  const handleIncrease = item => {
+    addToOrder(item, setOrder, order);
+  };
+
+  const handleSaveChanges = () => {
+    localStorage.setItem('Order', JSON.stringify(order));
+  };
+
+  useEffect(() => {
+    const quantityMap = order.reduce((acc, item) => {
+      acc[item._id] = item.quantity;
+      return acc;
+    }, {});
+    setQuantity(quantityMap);
+  }, [order]);
+
+  const getQuantity = itemId => {
+    return quantity[itemId] || 0;
+  };
+
   return (
     <>
       <h2>Shopping Cart</h2>
       {order.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul>
+        <OrderList>
           {order.map(item => (
             <li key={item._id}>
               <div>
@@ -44,18 +76,18 @@ export const ShoppingCart = () => {
                 />
                 <p>{item.name}</p>
                 <p>Quantity: {getQuantity(item._id)}</p>
+                <button onClick={() => handleDecrease(item)}>-</button>
+                <button onClick={() => handleIncrease(item)}>+</button>
                 <button onClick={() => handleDelete(item)}>Delete</button>
               </div>
             </li>
           ))}
-        </ul>
+        </OrderList>
       )}
-      <button
-        type="button"
-        onClick={() => {
-          clearCart();
-        }}
-      >
+      <button type="button" onClick={handleSaveChanges}>
+        Save Changes
+      </button>
+      <button type="button" onClick={clearCart}>
         Clear
       </button>
     </>
